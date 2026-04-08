2c66b8dd09e1bee7cfaf80329a8b758914a1ec59
Bishal-hub <bishal1999sahoo@gmail.com>
2026-04-08 22:22:40 +0530

    corrected terraform.yml file

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-locals {
+locals {                # locals = Local variables (temporary values inside Terraform)used to:Simplify logic, avoid repeating code, make configurations cleaner
   create_ec2 = var.stage == "ec2" || var.stage == "all"   # True if EC2 needs to be created
   create_s3  = var.stage == "s3" || var.stage == "all"    # True if S3 needs to be created
 }

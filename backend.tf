2ab76b8e123916017e2de02fbac481321196cd9d
Bishal-hub <bishal1999sahoo@gmail.com>
2026-04-28 02:55:07 +0000

    modified backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"   # S3 bucket to store state file
+    bucket         = "terraform-state-s3-bishal-bucket"   # S3 bucket to store state file
     key            = "terraform-project/terraform.tfstate" # Path inside bucket
     region         = "ap-south-1"                 # AWS region (Mumbai)
-    dynamodb_table = "terraform-lock-table"       # Table for state locking
+    dynamodb_table = "terraform-lock-table-bishal"       # Table for state locking
   }
 }
